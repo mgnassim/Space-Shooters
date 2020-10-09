895f56ab282a238663ab632f5a264c651594f567
Butros Groot <Butrosgroot@gmail.com>
2020-10-09 16:42:39 +0200

    website aan het maken

--- Space Shooters/Spaceshooters.py
+++ Space Shooters/Spaceshooters.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 
+GPIO.setmode(GPIO.BCM)
 
 HitPin = 15
 
@@ -15,7 +16,6 @@ servo3 = 6
 servo4 = 13
 servo5 = 19
 
-
+GPIO.setup(LOM1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 pin = GPIO.PWM(HitPin, 50)  # The GPIO pin dat aangestuurd moet worden, signaal frequentie
 pin.start(2.5)
-
