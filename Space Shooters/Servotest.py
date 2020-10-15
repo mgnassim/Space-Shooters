1a4398f40e7024caba1b3df9d2ce64a7e4271cfd
Z\bilal <bilal.balal.imtiaz@hva.nl>
2020-10-15 17:19:57 +0200

    no message

--- Space Shooters/Servotest.py
+++ Space Shooters/Servotest.py
@@ -1,10 +1,20 @@
 import RPi.GPIO as GPIO
+import time
 GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 GPIO.setup(11,GPIO.OUT)
-pwm=GPIO.PWM(11,50)
-pwm.start(0)
-pwm.ChangeDutyCycle(2)
-pwm.stop
+servo1=GPIO.PWM(11,50)#pin 11 heeft 50 herts
+servo1.start(2)
+time.sleep(1)
+
+servo1.ChangeDutyCycle(12)#rechts
+time.sleep(1)
+pwm.stop()
+print("doei")
+
+#pwm.ChangeDutyCycle(5)
+#pwm.ChangeDutyCycle(5)
+#pwm.ChangeDutyCycle(5)
 
 
 
