fbc1d48c46e24d9721ed0a7594f425f1f1a239b5
Butros Groot <Butrosgroot@gmail.com>
2020-10-17 11:52:36 +0200

    Website linken werkt tussen alles sites. Inlog scherm en taal keuze scherm werkt. alleen RFID sensor moet er nog aan gekopelt worden

--- Space Shooters/Website/SpaceShootersWeb.py
+++ Space Shooters/Website/SpaceShootersWeb.py
@@ -5,17 +5,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def link():
-    return redirect(url_for("login"))
+    return redirect(url_for("login_nl"))
 
 
-@app.route('/login/')
-def login():
+@app.route("/NL/login")
+def login_nl():
     return render_template("Space_Shooter_Web_Login_NL.html")
 
 
-@app.route('/language/')
+@app.route("/EN/login")
+def login_en():
+    return render_template("Space_Shooter_Web_Login_EN.html")
+
+
+@app.route("/language/")
 def language():
-    return render_template("Space_Shooter_Web_Login_NL.html")
+    return render_template("Space_Shooter_Web_language_selector.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
