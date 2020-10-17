0fb0c9cb907ec15c0c8b8e4e8b13e084a07ff4d8
Butros Groot <Butrosgroot@gmail.com>
2020-10-17 10:01:37 +0200

    heb de tweede web pagina gemaakt + rederect naar login van de / naar /login/ om het profresoneeler uit te laten zien

--- Space Shooters/Website/SpaceShootersWeb.py
+++ Space Shooters/Website/SpaceShootersWeb.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def index():
-    return render_template("Space_Shooter_Web_Taal_selector.html")
+def link():
+    return redirect(url_for("login"))
+
+
+@app.route('/login/')
+def login():
+    return render_template("Space_Shooter_Web_Login_NL.html")
+
+
+@app.route('/language/')
+def language():
+    return render_template("Space_Shooter_Web_Login_NL.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
