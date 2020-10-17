1a0732ef152180dcb4a0bd39da9310ba3f210616
Z\bilal <bilal.balal.imtiaz@hva.nl>
2020-10-17 19:54:23 +0200

    servomotor werkend draait 90 en terug en comments geplaats bi code

--- Space Shooters/Servotest.py
+++ Space Shooters/Servotest.py
@@ -4,17 +4,17 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 GPIO.setup(11,GPIO.OUT)
 servo1=GPIO.PWM(11,50)#pin 11 heeft 50 herts
-servo1.start(2)
+servo1.start(2)#gaat direct 90%
 time.sleep(1)
 
-servo1.ChangeDutyCycle(7)#rechts
-time.sleep(1)
-servo1.stop()
+servo1.ChangeDutyCycle(7)#brengt terug naar start plek
+time.sleep(5)
+#servo1.stop()
 print("doei")
 
 #pwm.ChangeDutyCycle(5)
 #pwm.ChangeDutyCycle(5)
 #pwm.ChangeDutyCycle(5)
-
+#
 
 
