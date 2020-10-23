64f62a36c4654c10ede51acb8e67df9c3125e7aa
Z\bilal <bilal.balal.imtiaz@hva.nl>
2020-10-23 16:24:35 +0200

    errro proberen te verhelpen

--- Space Shooters/i2ctest.py
+++ Space Shooters/i2ctest.py
@@ -1,9 +1,20 @@
-from adafruit_servokit import ServoKit
-kit = ServoKit(channels=16)
+import RPi.GPIO as GPIO
 import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(11,GPIO.OUT)
 
-kit.servo[0].angle = 90
-#time.sleep[50]
-kit.servo[0].angle = 0
 
-print("doei")
+servo1=GPIO.PWM(11,50)#pin 11 heeft 50 herts
+servo1.start(0)#draait direct 90 garden
+servo1.ChangeDutyCycle(8)  # brengt terug naar start plek
+servo1.ChangeDutyCycle(0)
+
+
+#servo1.stop()
+print("Test succesvol!!!!!!!!")
+
+#pwm.ChangeDutyCycle(5)
+#pwm.ChangeDutyCycle(5)
+#pwm.ChangeDutyCycle(5)
+#
