199c62a35e201846a057c36a8d0e5776cd9c16be
LAPTOP-PO6D9ULJ\NASSIM <nassim.mengat@hva.nl>
2020-11-12 15:11:26 +0100

    RFID last check; nu checked ie of de tag erbij hoort ofniet. Implementeren in game.py

--- Space Shooters/RFIDleesEnschrijf.py
+++ Space Shooters/RFIDleesEnschrijf.py
@@ -7,15 +7,16 @@ from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
 try:
-        text = input('New Data:')
-        print("Now place your tag to write")
-        reader.write(text)
-        print("Written")
-        id, text = reader.read()
-        print(id)
-        print(text)
+        print("Scan de RFID om te spelen!")
+        #readerWaarde leest af van reader.read de id van de tag
+        readerWaarde = reader.read()
+        #Dit doet wanneer er gescanned wordt en deze tag er echt bij hoort
+        #dat het dan uitvoert wat er in de if staat.
+        if readerWaarde[0] == 1093379580815:
+                print("Welkom bij Space Shooters!")
 
 # Finally betekent dat het de code toch uitvoert
 # maakt niet uit of try/except false of true is.
 finally:
         GPIO.cleanup()
+
