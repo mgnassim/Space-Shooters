a41eca4acad4b3b6fb00db0d31bdc00af2fd4b2b
Z\bilal <bilal.balal.imtiaz@hva.nl>
2020-11-29 02:45:26 +0100

    gewerkt aan een timer voor het spel en stopknop gebruikt gemaakt van HTML,CSS& JS

---
+++ Space Shooters/Website/static/Space_Shooter_Web_Gamepage_timer.js
@@ -0,0 +1,24 @@
+//const StartMinuten =1;
+let tijdLimiet = 60;
+
+const timer = document.getElementById("timer")// declareer timer met de waarde uit de HTML file met de aangegeven ID
+
+setInterval(updatetimer,1000);
+
+function updatetimer() {
+    //const minuten = Math.floor(time /60);
+    let seconden = tijdLimiet;//seconden gelijk aan tijdlimiet
+
+
+    timer.innerHTML = seconden;//verander de HTML waarde met de  met de gegeven waarde: seconden
+    tijdLimiet--; //seconden -1
+    if (seconden<=0){timer.innerHTML = "GAME OVER"}// als seconden onder of gelijk is aan 0 weergeef GAME OVER
+    StopKnop.addEventListener("click", Stoppen);
+
+
+}
+
+function Stoppen() {
+timer.innerHTML = "GAME STOPPED"
+
+}
